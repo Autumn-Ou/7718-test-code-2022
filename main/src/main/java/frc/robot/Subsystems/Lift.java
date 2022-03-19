6fab16a20707a1f7dfd46c7896310d1a8cf1c577
Autumn O <autumn-o@protonmail.com>
2022-03-18 21:09:20 -0400

    Lift Subsystem finished with conf data

--- main/src/main/java/frc/robot/Subsystems/Lift.java
+++ main/src/main/java/frc/robot/Subsystems/Lift.java
@@ -1,7 +1,37 @@
 package frc.robot.Subsystems;
 
 import frc.robot.RobotSubsystems;
+import com.ctre.phoenix.motorcontrol.VictorSPXControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 public class Lift extends RobotSubsystems {
+    /* 
+    The components relating to ONLY the Drive subsystem 
+    should be defined as class variables here 
+    */
+    public static VictorSPX liftMotor;
 
+    // Constructors
+    public Lift() {
+        robotInit();
+    }
+
+    public void robotInit() {
+        //.getProperty("name in config file", "backup value if config file not found or variable not found")
+        int liftMotorCanID = Integer.parseInt(
+            frc.robot.Robot.RobotConfiguration.getProperty("lift_motor_canid", "6"));
+
+        liftMotor = new VictorSPX(liftMotorCanID);
+    }
+
+    @Override
+    public void teleopPeriodic() {
+        if(frc.robot.Robot.driverTwo.getXButton())  {
+            liftMotor.set(VictorSPXControlMode.PercentOutput, 0.7);
+        }   else if(frc.robot.Robot.driverTwo.getYButton()) {
+            liftMotor.set(VictorSPXControlMode.PercentOutput, -0.7);
+        }   else    {
+            liftMotor.set(VictorSPXControlMode.PercentOutput, 0);
+        }
+    }
 }
