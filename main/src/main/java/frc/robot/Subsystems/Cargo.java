40eec69b63de14e0faba75ff2da2620b7cbb575b
Autumn O <autumn-o@protonmail.com>
2022-03-18 21:03:37 -0400

    Cargo Subsystem added and conf info added

--- main/src/main/java/frc/robot/Subsystems/Cargo.java
+++ main/src/main/java/frc/robot/Subsystems/Cargo.java
@@ -1,7 +1,48 @@
 package frc.robot.Subsystems;
 
 import frc.robot.RobotSubsystems;
+import com.ctre.phoenix.motorcontrol.VictorSPXControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 public class Cargo extends RobotSubsystems {
+    /* 
+    The components relating to ONLY the Drive subsystem 
+    should be defined as class variables here 
+    */
+    public static VictorSPX cargoArm;
+    public static VictorSPX cargoIntake;
 
+    // Constructors
+    public Cargo() {
+        robotInit();
+    }
+
+    public void robotInit() {
+        //.getProperty("name in config file", "backup value if config file not found or variable not found")
+        int cargoArmCanID = Integer.parseInt(
+            frc.robot.Robot.RobotConfiguration.getProperty("cargo_arm_canid", "4"));
+        int cargoIntakeCanID = Integer.parseInt(
+            frc.robot.Robot.RobotConfiguration.getProperty("cargo_intake_canid", "5"));
+
+        cargoArm = new VictorSPX(cargoArmCanID);
+        cargoIntake = new VictorSPX(cargoIntakeCanID);
+    }
+
+    public void teleopPeriodic()    {
+        if(frc.robot.Robot.driverOne.getAButton())  {
+            cargoArm.set(VictorSPXControlMode.PercentOutput, 0.3);
+        }   else if(frc.robot.Robot.driverOne.getBButton())   {
+            cargoArm.set(VictorSPXControlMode.PercentOutput, -0.3);
+        }   else  {
+            cargoArm.set(VictorSPXControlMode.PercentOutput, 0);
+        }
+
+        if(frc.robot.Robot.driverOne.getLeftBumper())  {
+            cargoIntake.set(VictorSPXControlMode.PercentOutput, 0.75);
+        }   else if(frc.robot.Robot.driverOne.getRightBumper()) {
+            cargoIntake.set(VictorSPXControlMode.PercentOutput, -0.75);
+        }   else    {
+            cargoIntake.set(VictorSPXControlMode.PercentOutput, 0);
+        }
+    }
 }
